cd4a94be5df275a7a63dece24955315844506121
deathgripsvegeta <sophiahowell831@gmail.com>
2022-10-03 11:28:53 -0700

    jkzxdfjksfjbsdgbfasj

---
+++ 2D Basic Movement/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    public float xRange = 8;
+
+    private float _horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * _horizontalInput * movementSpeed * Time.deltaTime);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }
+    }
+}
